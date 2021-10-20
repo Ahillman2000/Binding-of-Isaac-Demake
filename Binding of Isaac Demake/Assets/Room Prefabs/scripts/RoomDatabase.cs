1c587eca3a88399e671503c9a99682aecd9614e2
William <Hamish2.Lawrence@live.uwe.ac.uk>
2021-10-20 12:46:33 +0100

    fix merging pt1

---
+++ Binding of Isaac Demake/Assets/Room Prefabs/scripts/RoomDatabase.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomDatabase : MonoBehaviour
+{
+    public GameObject[] top_facing;
+    public GameObject[] bottom_facing;
+    public GameObject[] right_facing;
+    public GameObject[] left_facing;
+
+    public GameObject[] entry_room;
+    public GameObject closed_room;
+
+    public List<GameObject> rooms;
+    private int rand;
+
+    private void Start()
+    {
+        SpawnFirst();
+    }
+
+    void SpawnFirst()
+    {
+        rand = Random.Range(0, entry_room.Length);
+        Instantiate(entry_room[rand], transform.position, entry_room[rand].transform.rotation);
+    }
+}
